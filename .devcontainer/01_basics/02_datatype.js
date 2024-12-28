3443b2b3f9b5f07b90b7698e6a3138bc2ab8c2dd
Vivek Vishwakarma <96225464+vivek7897@users.noreply.github.com>
2024-12-28 10:33:17 +0000

    datatype

---
+++ .devcontainer/01_basics/02_datatype.js
@@ -0,0 +1,35 @@
+"use strict"; // treat all JS code as never version
+
+//alert( 3+3 ) we are using nodejs, not browser
+
+console.log(
+    
+    3+3)  // code readability should be high
+
+console.log("vivek");
+
+
+let name = " vivek"
+let age = 20
+
+let isLoggedIn = false
+
+// number => 2 to power 53
+//bigint
+//string=> ""
+//Boolean => true/false
+// null  standalone value  it give null value
+// undefined =>  value assign nhi hua hai
+// symbol  => unique
+
+
+// object 
+// null is a object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null);// object
+
+
+
+
+
