0cb32a51a16116f906dcd7e41f9c91760cd991e0
Vivek Vishwakarma <96225464+vivek7897@users.noreply.github.com>
2024-12-28 10:54:20 +0000

    conversion of datatype

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 144553
+let accountEmail = "vivekvish388@gmail.com"
+var accountPassword = "12345"
+accountCity = "Pune"
+let accountstate;
+
+
+//accountId = 2 // not allowed
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
+ /*
+ 
+ prefered not to use var
+ because of issue in block scope
+
+ */
---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+//let score = "vivek"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+//let valueInNumber = Number (score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+//"33"=>33
+//"33"=> NaN   ( not a number
+// true=>1; false=> 0
+
+let isLoggedIn = "vivek"
+
+let booleamIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleamIsLoggedIn);
+
+
+// 1=> true; 0 => false
+// "" => false
+// "vivek " => true
+
+
+let someNumber = 44
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+
+
+
+
